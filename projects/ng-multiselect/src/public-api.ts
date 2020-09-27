1dd37acb2dce4cc6b85d34ff31c2cab8592f9568
Vladimir Antin <antin502@gmail.com>
2020-09-27 16:54:19 +0200

    N

--- projects/ng-multiselect/src/public-api.ts
+++ projects/ng-multiselect/src/public-api.ts
@@ -2,5 +2,7 @@
  * Public API Surface of ng-multiselect
  */
 
+export * from './lib/truncate.pipe';
+export * from './lib/selected-item/selected-item.component';
 export * from './lib/ng-multiselect.component';
 export * from './lib/ng-multiselect.module';
